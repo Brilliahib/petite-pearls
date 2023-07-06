f150525bf9419e3896ba48d4b916f216d93154c8
Brilliahib <brilliahib21@gmail.com>
2023-07-06 19:53:25 +0700

    update detail produk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "detailProduk.html", "detailProduk2.html", "detailProduk3.html"],
   theme: {
     container: {
       center: true,
